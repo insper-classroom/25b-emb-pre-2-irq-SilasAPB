d23a0018cd57cc8053c91c351737b84fc73b3ada
Silas Pereira <silasapb@al.insper.edu.br>
2025-08-22 13:47:49 -0300

    ex1_1

--- exe1/main.c
+++ exe1/main.c
@@ -3,12 +3,14 @@
 #include <stdio.h>
 
 const int BTN_PIN_R = 28;
+volatile b_status =0;
+
 
 void btn_callback(uint gpio, uint32_t events) {
   if (events == 0x4) { // fall edge
-    printf("fall \n");
+    b_status =1;
   } else if (events == 0x8) { // rise edge
-    printf("rise \n");
+    
   }
 }
 
@@ -23,5 +25,11 @@ int main() {
       BTN_PIN_R, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true, &btn_callback);
 
   while (true) {
+    if (b_status){
+      b_status = 0; // clean IRS flag
+      printf("fall \n");
+    }else{
+      printf("rise \n");
+    }
   }
 }
